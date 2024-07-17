28dd5e071e3c2d72f9cd6964af786162de6344ed
Soumodip Das <soumodipd567@gmail.com>
2024-07-17 16:48:04 +0530

    done

--- client/components/CreateBlog.jsx
+++ client/components/CreateBlog.jsx
@@ -1,55 +1,91 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import uploadGif from "../src/assets/uploadVid.mp4"; // Adjust the path accordingly
 
 const CreateBlog = () => {
   const [file, setFile] = useState(null);
-  const [title, setTitle] = useState('');
-  const [details, setDetails] = useState('');
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
   const navigate = useNavigate();
 
   const handleUpload = () => {
     const formData = new FormData();
-    formData.append('file', file);
-    formData.append('title', title);
-    formData.append('details', details);
+    formData.append("file", file);
+    formData.append("title", title);
+    formData.append("details", details);
 
     axios
-      .post('http://localhost:4000/upload', formData)
+      .post("http://localhost:4000/upload", formData)
       .then((res) => {
         console.log(res);
-        navigate('/');
+        navigate("/");
       })
       .catch((err) => console.log(err));
   };
 
   return (
-    <div>
-      <label htmlFor="title">Blog Title: </label>
-      <input
-        type="text"
-        placeholder="Write Blog Title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
+    <div className="create-blog-container">
+      <h1>Create a New Blog</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-field">
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="input-area"
+              required
+            />
+            <label htmlFor="title" className="label-area">
+              Write Blog Title:{" "}
+            </label>
+          </div>
+        </div>
 
-      <label htmlFor="details">Blog Details:</label>
-      <textarea
-        placeholder="Write Blog Details"
-        value={details}
-        onChange={(e) => setDetails(e.target.value)}
-        required
-      ></textarea>
+        <div className="form-group">
+          <div className="input-field">
+            <textarea
+              rows={10}
+              id="details"
+              value={details}
+              className="input-area"
+              onChange={(e) => setDetails(e.target.value)}
+              required
+            ></textarea>
+            <label htmlFor="details" className="label-area">
+              Write Blog Details:
+            </label>
+          </div>
+        </div>
 
-      <label htmlFor="image">Add a Blog Image: </label>
-      <input
-        type="file"
-        onChange={(e) => setFile(e.target.files[0])}
-        required
-      />
-      
-      <button onClick={handleUpload}>Upload</button>
+        <div className="file-form-group">
+  <div className="file-input-field">
+    <p>Upload Blog Image</p>
+    <label htmlFor="image" className="file-label-area">
+      <video width="200" height="200" className="upload-video" autoPlay loop muted>
+        <source src={uploadGif} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </label>
+    <input
+      type="file"
+      id="image"
+      className="file-input-area"
+      onChange={(e) => setFile(e.target.files[0])}
+      required
+    />
+  </div>
+</div>
+
+
+        <div className="btn">
+          <button type="button" onClick={handleUpload}>
+            Upload
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,15 +3,19 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
+      <div className="navbar-brand">
+        <img src="../src/assets/blog.png" alt="Blog-Logo" className="navbar-icon" title='Blog-icon'/>
+        <span className="navbar-text" title='Blog-text'>Blog Website</span>
+      </div>
       <ul>
         <li>
-          <Link to="/" className="link">All Blogs</Link>
+          <Link to="/">All Blogs</Link>
         </li>
         <li>
-          <Link to="/create" className="link">Create Blog</Link>
+          <Link to="/create">Create Blog</Link>
         </li>
       </ul>
-      <div className="animation start-home"></div>
+      <span className="animation"></span>
     </nav>
   );
 };
